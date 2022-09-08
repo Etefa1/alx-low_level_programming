05895e18ea25ade6b7037e288e908432fcec22cb
Etefa1 <Etefaangasu@gmail.com>
2022-09-08 16:00:48 -0700

    epha

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 char c;
 int i;
-long int li;
-long long int lli;
+long int l;
+long long int ln;
 float f;
-printf("Size of a char: %ld byte(s),\n", sizeof(c));
-printf("Size of an int: %ld byte(s),\n", sizeof(i));
-printf("size of a long int: %ld byte(s),\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s),\n", sizeof(lli));
-printf("size of a float: %ld byte(s),\n", sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
