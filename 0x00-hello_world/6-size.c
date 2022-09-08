a63067bef230a94dda27e0b49f3b3f35355c27f4
Etefa1 <Etefaangasu@gmail.com>
2022-09-08 15:43:32 -0700

    good gob

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-char c; 
+char c;
 int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %lu byte(s),\n", sizeof(c)); 
+printf("Size of a char: %lu byte(s),\n", sizeof(c));
 printf("Size of an int: %lu byte(s),\n", sizeof(i));
 printf("size of a long int: %lu byte(s),\n", sizeof(li));
 printf("Size of a long long int: %lu byte(s),\n", sizeof(lli));
