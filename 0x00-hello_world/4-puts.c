93e5b0c3aacabc62e5c6adc32c6d60e2056acb45
Etefa1 <Etefaangasu@gmail.com>
2022-09-08 13:44:20 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#include<stdio.h>
-int puts(""Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+int main(void){
+	/*programming today 
+	 * is just
+	 * cool*/
+puts("\n"Programming is like building a multilingual puzzle");
+return(0)
+}
