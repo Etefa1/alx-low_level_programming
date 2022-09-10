6c0a52c75e0ba8a3c4eeff2abaf31e50fd865ea5
Etefa1 <Etefaangasu@gmail.com>
2022-09-09 18:51:20 -0700

    great my God

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers
+ * All your code in the main function
+ * Return: 0
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+	putchar('i');
+	i += 1;
+}
+putchar('\n');
+return (0);
+}
