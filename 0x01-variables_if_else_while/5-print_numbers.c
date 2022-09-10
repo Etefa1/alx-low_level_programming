2e1b4624bc29e4e5d00756b4f136cb1f6c11cdc7
Etefa1 <Etefaangasu@gmail.com>
2022-09-09 18:15:42 -0700

    personal work

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase exept e and q
+ * You can only use the putchar
+ * Return: 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 9)
+
+{
+printf("\n");
+n++;
+}
+return (0);
+}
