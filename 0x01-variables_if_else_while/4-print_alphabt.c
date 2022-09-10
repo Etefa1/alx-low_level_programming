b5e9e07d43fb7de4c91702fe04e007d75e5b8d88
Etefa1 <Etefaangasu@gmail.com>
2022-09-09 17:41:33 -0700

    do it

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase exept e and q
+ * You can only use the putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int low = 97;
+
+	while (low <= 122)
+	{
+
+		if (low == 'e' || low == 'q')
+		{
+			low += 1;
+		}
+		else
+		{
+			putchar(low);
+			low <= 1;
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
