c6492b2a1947f7e494515b7bfdc2325cd98308b9
Etefa1 <Etefaangasu@gmail.com>
2022-09-13 15:46:45 -0700

    my work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void);
+return (0);
+}
+{
+int x = 97;
+int i = 122;
+}
+{
+	while (x <= i; x++)
+		_putchar (x);
+	_putchar  ('\n');
+}
