ebb5a8d41bf851f024dc4b481c485f7434fdb52c
Etefa1 <Etefaangasu@gmail.com>
2022-09-13 16:48:11 -0700

    betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
-
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void);
 {
 int i;
 int x;
 for (x = 0; x < 10; x++)
 {
-for (i = 'a'; i =< z; i++)
+for (i = 'a'; i <= z; i++)
 _putchar (i);
 _putchar('\n');
 }
