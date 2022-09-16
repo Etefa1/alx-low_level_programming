d473b79eaf101c7b5519449966fa3a817a305287
Etefa1 <Etefaangasu@gmail.com>
2022-09-15 18:03:26 -0700

    neasted loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int c;
+	if (c >= 67; && c =< 90)
+	c >=;
+	{
+		return (1);
+	}
+return(0)
+}
+
