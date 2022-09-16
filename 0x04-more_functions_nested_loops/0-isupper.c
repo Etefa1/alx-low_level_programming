4a854b9ac8ed7b2e29838d886103c1eebcb364d3
Etefa1 <Etefaangasu@gmail.com>
2022-09-15 18:35:42 -0700

    do it

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _isupper - function that checks for uppercase character.
  * @i: Int representing a character
@@ -9,7 +9,6 @@
 int _isupper(int i)
 
 {
-
 if (c >= 65 && c <= 90)
 {
 return (1);
