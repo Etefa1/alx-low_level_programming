fe922d176c9c0f5b12b8c1599414ebd6fdc8e282
Etefa1 <Etefaangasu@gmail.com>
2022-09-22 15:46:04 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strcmp -  function that compares two strings.
+ * @s1: primera cadena.
+ * @s2: segunda cadena.
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+strcmp(s1, s2);
+return (0);
+}
