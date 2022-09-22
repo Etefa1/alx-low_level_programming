4afd183358ce0cb5e412a37760d0e8248be1d563
Etefa1 <Etefaangasu@gmail.com>
2022-09-22 14:57:41 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _strcat - function that cocatinates two strings.
+ *
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	ctrcat(dest, src);
+		return (dest);
+}
