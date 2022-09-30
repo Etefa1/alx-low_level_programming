b5643bfc423adaba515891d1b4a1ccdc3d371330
Etefa1 <Etefaangasu@gmail.com>
2022-09-30 15:39:11 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -  writes program name
+* @argc: arguement
+* @argv: array of pointers
+* Return: 0
+**/
+int main(int argc __attribute__((unused)), char **argv)
+	printf("%s\n", argv[0]);
+	return (0);
